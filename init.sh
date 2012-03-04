5b719445a7d81b8e0b9c11cafbb455ff693f1395
Jasper Li <jasper.li@vonbros.com>
2012-03-04 16:53:39 +0800

    update local pip dir automatically

--- init.sh
+++ init.sh
@@ -6,15 +6,19 @@ PWD=`pwd`
 PWD=`dirname $PWD`
 
 
-chmod +w /etc/sudoers
+#chmod +w /etc/sudoers
 #echo "$MYID ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-chmod -w /etc/sudoers
+#chmod -w /etc/sudoers
 
 cp -f ./stackrc_template ./stackrc
 sed -e "s|%BASESRC%|$PWD|g" -i ./stackrc
+
+cp -f ./functions_template ./functions
+sed -e "s|%PIPLOCALCACHE%|$PWD/cache/pip|g" -i ./functions
+
 #sed -e "s|%BASESRC%|$PWD|g" -i ./restart_stack.sh
-apt-get update
-cp -rf $PWD/cache/apt /var/cache/
+#apt-get update
+#cp -rf $PWD/cache/apt /var/cache/
 
 
 echo "Done"
